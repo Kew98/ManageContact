5262693e04fa2f3f71abd49243bb5bd6f0f564c6
Kevin Wen <kew98@yahoo.com>
2021-06-03 11:16:17 -0400

    Manage Contact RESTful service

---
+++ ManageContact/Controllers/Contact.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageContact.Controllers
+{
+	//"type": string["home" | "work" | "mobile"]
+
+	public class Contact
+	{
+		public int Id { get; set; }
+		public Name name { get; set; }
+		public Address address { get; set; }
+		public Phone[] phone { get; set; }
+		public string email { get; set; }
+
+	}
+
+	public class Name
+	{
+		public string first { get; set; }
+		public string middle { get; set; }
+		public string last { get; set; }
+	}
+
+	public class Address
+	{
+		public string street { get; set; }
+		public string city { get; set; }
+		public string state { get; set; }
+		public string zip { get; set; }
+	}
+
+	public class Phone
+	{
+		public string number { get; set; }
+		public string type { get; set; }
+
+	}
+
+	public class CallContact
+	{
+		public Name name { get; set; }
+		public string phone { get; set; }
+	}
+}
---
+++ ManageContact/Controllers/ContactsController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiteDB;
+using Newtonsoft.Json;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ManageContact.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactsController : ControllerBase
+	{
+		private string DBNAME = @"..\ManageContact\Database\MyLiteData.db";
+
+		// phone: "type": string ["home" | "work" | "mobile"]
+		// GET: api/<ContactsController>
+		[HttpGet]
+		public IEnumerable<Contact> Get()
+		{
+			using (var db = new LiteDatabase(DBNAME))
+			{
+				var Contacts = db.GetCollection<Contact>("Contact");
+
+				var list = Contacts.Query().ToList();
+
+				return list;
+			}
+
+		}
+
+		// GET api/<ContactsController>/5
+		[HttpGet("{id}")]
+		public Contact Get(int id)
+		{
+			using (var db = new LiteDatabase(DBNAME))
+			{
+				var Contacts = db.GetCollection<Contact>("Contact");
+
+				var contact = Contacts.FindById(id);
+
+				return contact;
+			}
+		}
+
+		// GET api/<ContactsController>/5
+		[HttpGet("call-list")]
+		public string GetCallList()
+		{
+			using (var db = new LiteDatabase(DBNAME))
+			{
+				var Contacts = db.GetCollection<Contact>("Contact");
+
+				List<CallContact> list = new List<CallContact>();
+
+				foreach (var contact in Contacts.Query().ToList())
+				{
+					CallContact callContact = new CallContact();
+					callContact.name = contact.name;
+
+					var phone = contact.phone.Where(a => a.type == "home").FirstOrDefault();
+					if (phone != null)
+					{
+						callContact.phone = phone.number;
+					}
+
+					list.Add(callContact);
+				}
+
+				string JSONresult = JsonConvert.SerializeObject(list);
+				return JSONresult;
+			}
+		}
+
+		// POST api/<ContactsController>
+		[HttpPost]
+		public void Post([FromBody] Contact contact)
+		{
+			using (var db = new LiteDatabase(DBNAME))
+			{
+				var Contacts = db.GetCollection<Contact>("Contact");
+
+				Contacts.Insert(contact);
+			}
+
+		}
+
+		// PUT api/<ContactsController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] Contact contactUpdate)
+		{
+			using (var db = new LiteDatabase(DBNAME))
+			{
+				var Contacts = db.GetCollection<Contact>("Contact");
+
+				var contact = Contacts.FindById(id);
+
+				contact.name = contactUpdate.name;
+				contact.address = contactUpdate.address;
+				contact.phone = contactUpdate.phone;
+				contact.email = contactUpdate.email;
+
+				Contacts.Update(contact);
+			}
+		}
+
+		// DELETE api/<ContactsController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			using (var db = new LiteDatabase(DBNAME))
+			{
+				var Contacts = db.GetCollection<Contact>("Contact");
+
+				Contacts.Delete(id);
+			}
+		}
+	}
+}
